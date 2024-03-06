b36d07721864a8fe26ff5a3edfe7352873901ef7
Ashoka1852 <122711342+Ashoka1852@users.noreply.github.com>
2024-03-06 13:55:41 -0800

    Create Day1_Codecademy.js

---
+++ Day1_Codecademy.js
@@ -0,0 +1,59 @@
+//Commenting is a core concept
+
+//Single Line Comment
+/* 
+
+this is an example of a multiline
+comment.
+
+*/
+
+//prints to the console
+console.log(90);
+
+
+
+//Examples of primative data types
+
+console.log('JavaScript'); // String
+console.log(2011); //number
+console.log('Woohoo! I love to code! #codecademy');
+console.log(20.49);
+
+//Examples of Arithmetic Operators: 
+console.log(3.5+18);
+console.log(2021-1969);
+console.log(65/240);
+console.log(.2708*100);
+
+console.log(3 + 4); // Prints 7
+console.log(5 - 1); // Prints 4
+console.log(4 * 2); // Prints 8
+console.log(9 / 3); // Prints 3
+
+//examples of string concatination
+console.log('Hello' + 'World');
+console.log('Hello' + ' '+ 'World');
+
+
+console.log('Hello' + ' ' + 'World!');
+
+
+// Use .toUpperCase() to log 'Codecademy' in all uppercase letters
+console.log('Codecademy');
+console.log('Codecademy'.toUpperCase());
+
+// Use a string method to log the following string without whitespace at the beginning and end of it.
+console.log('    Remove whitespace   ');
+console.log('    Remove whitespace   '.trim());
+
+
+//Built in objects: 
+console.log(Math.random()* 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(43.8));
+console.log(Number.isInteger(2017));
+
+
+
+
